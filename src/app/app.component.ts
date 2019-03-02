96fde5702e792058dc886abd772afa8fe10273a5
chenjingyue <1544552008@qq.com>
2019-03-02 14:55:50 +0800

    commit001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo01';
+
+  tableNames:any;
+  tableValues:any;
+  tableStyle:any;
+  width:string = "100px";
+  height:string = "100%";
+  ngOnInit() {
+    this.initTableParam();
+  }
+  initTableParam() {
+    this.tableNames = [
+      "id",
+      "name",
+      "age",
+      "class",
+      "sex",
+      "Hobby"
+    ];
+    this.tableValues = [];
+    for (let index = 0; index < 10; index++) {
+      let object = {
+        'id': index,
+        'name': `name+ ${index}`,
+        'age': `age+ ${index}`,
+        'class': `class+ ${index}`,
+        'sex': `sex+ ${index}`,
+        'Hobby': `'Hobby'+ ${index}`
+      }
+      this.tableValues.push(object);
+    }
+    this.tableStyle = [
+      { key:'id',
+        width:'100px',
+        type:'text'
+      },
+      { key:'name',
+        width:'100px',
+        type:'text'
+      },
+      { key:'age',
+        width:'100px',
+        type:'text'
+      },
+      { key:'class',
+        width:'100px',
+        type:'text'
+      },
+      { key:'sex',
+        width:'100px',
+        type:'text'
+      },
+      { key:'Hobby',
+        width:'100px',
+        type:'text'
+      },
+      {
+        key:'operate',
+        width:'120px',
+        type:'operate',
+        val:[
+          {
+            name:'编辑',
+            type:'edit'
+          },
+          {
+            name:'删除',
+            type:'delete'
+          }
+        ]
+      }
+    ];
+  }
+
 }
